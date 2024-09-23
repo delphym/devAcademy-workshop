ca78c4ac51e10461b034435773974c6abe3d0199
delphym <dmladek@gmail.com>
2024-09-23 21:20:23 +1200

    Added Node.JS and CommonJS original module system
    - provided and example of using modern modules with Node.JS

---
+++ src/modules-excercise/commonJS/hype-the-crowd.mjs
@@ -0,0 +1,9 @@
+// hype-the-crowd.mjs
+export default function hypeTheCrowd(call, response) {
+  console.log(`When I say ${call}, you say ${response}!`)
+  console.log(`ME: ${call.toUpperCase()}!`)
+  console.log(`CROWD: ${response.toUpperCase()}!`)
+  console.log(`ME: ${call.toUpperCase()}!`)
+  console.log(`CROWD: ${response.toUpperCase()}!`)
+  console.log(`ME: I can't hear you!`)
+}
---
+++ src/modules-excercise/commonJS/main.mjs
@@ -0,0 +1,6 @@
+// main.mjs
+// run it as: 'node main.mjs'
+import hypeTheCrowd from './hype-the-crowd.mjs'
+
+hypeTheCrowd('cheese', 'cake')
+hypeTheCrowd('hot', 'dog')
